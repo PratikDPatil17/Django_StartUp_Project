d58839ff19e5e47d7624d6923c491048f1e161f2
PratikDPatil17 <pratikpatil.dtis@gmail.com>
2018-12-01 15:45:29 +0530

    Fetch data for listings and single listing page

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listing
 
 # Create your views here.
+# we fetch our listings using model.py and then insert it into template and then loop through database
 def index(request):
-    return render(request, 'listings/listings.html')
+    #listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+    #fetching from database
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404 (Listing, pk = listing_id)
+
+    context={
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
